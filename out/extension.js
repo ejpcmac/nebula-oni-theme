3d06a5c8e39a5448b8918748dd599e5733eca937
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2023-05-04 23:48:57 +0200

    remove unneeded activation code

--- out/extension.js
+++ out/extension.js
@@ -1,79 +1,8 @@
 "use strict"
 
-const vscode = require("vscode")
+function activate() { }
 
-const {
-	Theme
-} = require("../src/_make-theme/_make-theme")
-
-const {
-	markdownFile,
-	themeMenuName
-} = require("../data/theme-names")
-
-const {
-	info,
-	installMessage,
-	reloadMessage,
-	updateMessage,
-	website,
-} = require("../src/vscode/info-message")
-
-const {
-	favoriteConfig,
-	saveSettings,
-	themeConfig,
-	freshInstall,
-} = require("../src/vscode/user-preferences")
-
-const {
-	applyChanges,
-	applyChangesFalse,
-	favoriteOFF,
-	getConfig,
-} = require("../src/vscode/menu-settings")
-
-const {
-	createFile,
-	dataDir,
-	dataFileName,
-	fileMessage,
-	noFile,
-} = require("../src/vscode/theme-control")
-
-// ! for tests
-// info("FIRST")
-// info(getConfig(), "OK")
-// info(`APPLY CHANGES: ${applyChanges()}`)
-
-function activate() {
-	if (noFile) {
-		createFile(dataDir, dataFileName, markdownFile, fileMessage)
-		if (freshInstall) {
-			info(installMessage, "More Info", website)
-			saveSettings(getConfig())
-		} else {
-			info(updateMessage, "OK")
-			new Theme(themeConfig()).allThemes()
-			new Theme(favoriteConfig()).favoriteTheme()
-		}
-	}
-	vscode.workspace.onDidChangeConfiguration((event) => {
-		if (event.affectsConfiguration(themeMenuName)) {
-			if (applyChanges()) {
-				const config = getConfig()
-				new Theme(config).allThemes()
-				new Theme(config).favoriteTheme()
-				applyChangesFalse()
-				favoriteOFF()
-				info(reloadMessage, "Reload", true)
-				saveSettings(config)
-			}
-		}
-	})
-}
-
-function deactivate() {}
+function deactivate() { }
 
 module.exports = {
 	activate,
